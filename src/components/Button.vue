7ed8bc0150d1d40f60ac0034828085c62a8246ab
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-11-25 01:10:57 +0900

    added displaying next video after another and button component

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    :id="id"
+    class="group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black"
+    :class="containerClass"
+  >
+    <slot name="leftIcon"></slot>
+    <span
+      class="relative incline-flex overflow-hidden font-general text-xs uppercase"
+      ><div>{{ title }}
+        
+      </div></span
+    >
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["id", "title", "containerClass"],
+};
+</script>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,22 +1,26 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import Button from "./Button.vue";
+import { Icon } from "@iconify/vue";
 
 const currentIndex = ref(1);
 const hasClicked = ref(false);
 const isLoading = ref(true);
 const loadedVideo = ref(0);
 
-const totalVideo = 4;
-const nexVideo = ref(null);
+const totalVideo = 3;
+const nextVideo = ref(null);
 
 const handleVideoLoad = () => {
-  loadedVideo.value++
+  loadedVideo.value++;
 };
 
-function handleMiniVidClick () {
+const upcomingVideo = computed(() => (currentIndex.value % totalVideo) + 1);
+
+const handleMiniVidClick = () => {
   hasClicked.value = true;
 
-  currentIndex.value = (currentIndex.value % totalVideo) + 1;
+  currentIndex.value = upcomingVideo.value;
 };
 
 const videoSrc = (index) => `/videos/hero-${index}.mp4`;
@@ -37,8 +41,8 @@ const videoSrc = (index) => `/videos/hero-${index}.mp4`;
             class="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
           >
             <video
-              :src="videoSrc(currentIndex)"
-              ref="nexVideo"
+              :src="videoSrc(upcomingVideo)"
+              ref="nextVideo"
               loop
               muted
               id="current-video"
@@ -46,6 +50,49 @@ const videoSrc = (index) => `/videos/hero-${index}.mp4`;
               @loadeddata="handleVideoLoad"
             ></video>
           </div>
+        </div>
+        <video
+          ref="nextVideo"
+          :src="videoSrc(currentIndex)"
+          loop
+          muted
+          id="next-video"
+          class="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+          @loadeddata="handleVideoLoad"
+        ></video>
+        <video
+          :src="videoSrc(currentIndex)"
+          loop
+          muted
+          class="absolute left-0 top-0 size-full object-cover object-center"
+          @loadeddata="handleVideoLoad"
+        ></video>
+      </div>
+      <h1
+        class="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75"
+      >
+        G<b>a</b>ming
+      </h1>
+      <div class="absolute left-0 top-0 z-40 size-full">
+        <div class="mt-24 px-5 sm:px-10">
+          <h1 class="special-font hero-heading text-blue-100">
+            redefi<b>n</b>e
+          </h1>
+          <p class="mb-5 max-w-64 font-robert-regular text-blue-100">
+            Enter MetaGame layer <br />
+            Unleash the Play Economy
+          </p>
+          <Button
+            id="watch-trailer"
+            title="Watch Trailer"
+            containerClass="!bg-yellow-300 flex-center gap-1"
+            ><template #leftIcon>
+              <Icon
+                icon="iconamoon:arrow-top-right-2-bold"
+                width="20"
+                height="24"
+              /> </template
+          ></Button>
         </div>
       </div>
     </div>
