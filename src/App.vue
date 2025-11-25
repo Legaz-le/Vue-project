ae576dd4dbf31a7f5defe7faf523724caec6075b
legaz <188694281+Legaz-le@users.noreply.github.com>
2025-11-26 02:38:44 +0900

    made about section

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-import Hero from './components/Hero.vue'
+import Hero from "./components/Hero.vue";
+import About from "./components/About.vue";
 </script>
 
-<template class="relative min-h-screen w-screen overflow-x-hidden">
-  <Hero />
-  <section  class="z-0 min-h-screen bg-blue-500"/>
+<template>
+  <div class="relative min-h-screen w-screen overflow-x-hidden">
+    <Hero />
+    <About />
+  </div>
 </template>
---
+++ src/components/About.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { watch, onMounted } from "vue";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted(() => {
+  const clipAnimation = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#clip",
+      start: "center center",
+      end: "+=800 center",
+      scrub: 0.5,
+      pin: true,
+      pinSpacing: true,
+    },
+  });
+
+  clipAnimation.to(".mask-clip-path", {
+    width: "100vw",
+    height: "100vh",
+    borderRadius: 0,
+  });
+});
+</script>
+
+<template lang="">
+  <div id="about" class="min-h-screen w-screen ">
+    <div class="relative mb-8 mt-36 flex flex-col items-center gap-5">
+      <p class="font-general text-sm uppercase md:text-[10px]">
+        Welcome to Zentry
+      </p>
+      <div
+        class="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]"
+      >
+        Disc<b>o</b>ver the world's <br />
+        <b>l</b>argest shared adventure
+      </div>
+      <div class="about-subtext">
+        <p>The Game of Games begins-your life, now an epic MMORPG</p>
+        <p>Zentry unites every player from countless games and platforms</p>
+      </div>
+    </div>
+    <div id="clip" class="h-dvh w-screen relative">
+      <div class="mask-clip-path about-image">
+        <img
+          src="/img/about.webp"
+          alt="Background"
+          class="absolute left-0 top-0 size-full object-cover"
+        />
+      </div>
+    </div>
+  </div>
+</template>
